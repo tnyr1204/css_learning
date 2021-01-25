63b782ae2fd9c0a5b88012f5b41f1b0244bd0796
DESKTOP-36SRR8F\unita <unitary580@gmail.com>
2021-01-26 00:25:44 +0900

    - ハンバーガーメニューサンプル追加 - h2の装飾追加

--- js/test.js
+++ js/test.js
@@ -1,21 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(){
-var btn01 = document.getElementById('btn01');
+  var btns = document.querySelectorAll('.btn-trigger');
+  const nav = document.getElementById('nav');
 
-btn01.addEventListener('click', function() {
-  if (btn01.classList.contains('active')) {
-    btn01.classList.remove("active");
-  } else {
-    btn01.classList.add("active");
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener('click', function() {
+      this.classList.toggle("active");
+      nav.classList.toggle("open");
+    }, false);
   }
-});
-
-var btn02 = document.getElementById('btn02');
-
-btn02.addEventListener('click', function() {
-  if (btn02.classList.contains('active')) {
-    btn02.classList.remove("active");
-  } else {
-    btn02.classList.add("active");
-  }
-});
 }, false);
